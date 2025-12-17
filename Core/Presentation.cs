374de88cd779f62bc7e57c82e64d86a65fc1b114
alexcoroi <a.coroi@students.htl-leonding.ac.at>
2025-12-17 20:37:35 +0100

    created model

---
+++ Core/Presentation.cs
@@ -0,0 +1,9 @@
+﻿namespace Core;
+
+public record class Presentation
+{
+    public required string Name { get; set; }
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+    public required string Location { get; set; }
+}
